40cd439a3cfd24e82943af87d183911797ff0c62
Tomás Joaquín Antileo Jara <tomas.antileo1801@alumnos.ubiobio.cl>
2022-05-25 17:06:10 -0400

    conexion servidor

---
+++ partes/conexion.php
@@ -0,0 +1,14 @@
+<?php 
+$hostname = "146.83.194.142";
+$username = "E2software";
+$password = "E2software1113";
+$database = "E2software_bd";
+$conn;
+try {
+  $conn = new PDO("mysql:host=$hostname;dbname=$database", $username, $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  //echo "Connected successfully";
+} catch (PDOException $e) {
+  //echo "Connection failed: " . $e->getMessage();
+}
