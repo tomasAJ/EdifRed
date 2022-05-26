a1ba8a4a162a11b024e97a6883a2d294ae6ac056
Tomás Joaquín Antileo Jara <tomas.antileo1801@alumnos.ubiobio.cl>
2022-05-26 01:17:51 -0400

    ruta

--- partes/nav.php
+++ partes/nav.php
@@ -1,5 +1,5 @@
 <!-- Navbar -->
-    <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
+<nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
         <div class="container">
         
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
@@ -14,22 +14,23 @@
                 </form>
                 <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
                     <li class="nav-item dropdown">
-                        <a class="nav-link text-dark dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                        <a class="nav-link text-dark dropdown-toggle" href="" id="navbarDropdown" role="button"
                         data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                         <img src="https://previews.123rf.com/images/jemastock/jemastock1609/jemastock160905175/63043795-persona-ejecutiva-en-traje-con-la-ilustraci%C3%B3n-de-vector-de-imagen-de-iconos-de-negocios-relacionados-co.jpg" 
                         class="img-fluid rounded-circle avatar mr-2"
-                        alt="imagen" />    
+                            
+                        alt="imagen" /> 
                         <?php echo $_SESSION['nombreusuario'];?>
                         </a>
                             <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="../perfil/"><i class="fas fa-user"></i> Mi perfil</a>
+                                <a class="dropdown-item" href="../perfil"><i class="fas fa-user"></i> Mi perfil</a>
                                 
                             <div class="dropdown-divider"></div>
-                                <a class="dropdown-item" href="cerrar.php"><i class="fas fa-sign-out-alt"></i> Cerrar sesión</a>
+                                <a class="dropdown-item" href="../partes/cerrar.php"><i class="fas fa-sign-out-alt"></i> Cerrar sesión</a>
                             </div>
                     </li>
                     <li class="nav-item dropdown">
-                        <a class="nav-link text-dark dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                        <a class="nav-link text-dark dropdown-toggle" href="" id="navbarDropdown" role="button"
                         data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                         mensaje
                         </a>
